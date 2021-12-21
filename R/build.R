4528d0843532eae32aca089d5e869b537454ba82
graymarymack <graymarymack@gmail.com>
2021-12-21 17:06:48 -0500

    First commit web

---
+++ R/build.R
@@ -0,0 +1,5 @@
+# An optional custom script to run before Hugo builds your site.
+# You can delete it if you do not need it.
+
+#run this line if needed when adding .rmd file to site that is not compatible with blogdown (e.g. resume)
+#blogdown::build_dir('static')
